d73758bcb91deaf4b0da74d81220194c4fb86791
MonssafToukal <MonssafToukal@users.noreply.github.com>
2022-01-28 20:44:03 +0000

    :robot: Format .jl files

--- src/workers.jl
+++ src/workers.jl
@@ -6,14 +6,14 @@ const QSUB_FLAGS = `-q hs22 -V`
 const EXE_FLAGS = "--project=."
 const WORKING_DIRECTORY = joinpath(ENV["HOME"], "julia_worker_logs")
 
-function setup_workers(;nb_nodes=MAX_NUMBER_SGE_NODES, qsub_flags=QSUB_FLAGS, exe_flags=EXE_FLAGS, wd=WORKING_DIRECTORY)
+function setup_workers(;
+  nb_nodes = MAX_NUMBER_SGE_NODES,
+  qsub_flags = QSUB_FLAGS,
+  exe_flags = EXE_FLAGS,
+  wd = WORKING_DIRECTORY,
+)
   @assert nb_nodes ≤ MAX_NUMBER_SGE_NODES "Number of nodes requested exceeds $MAX_NUMBER_SGE_NODES"
-  addprocs_sge(
-    nb_nodes;
-    qsub_flags=qsub_flags,
-    exeflags=exe_flags,
-    wd = wd,
-  )
+  addprocs_sge(nb_nodes; qsub_flags = qsub_flags, exeflags = exe_flags, wd = wd)
 end
 
 function dispatch_modules()
@@ -23,11 +23,11 @@ function dispatch_modules()
   end
 end
 
-function init_workers(nb_retries=5;kwargs...)
-  let task = Task(() -> setup_workers(;kwargs...))
+function init_workers(nb_retries = 5; kwargs...)
+  let task = Task(() -> setup_workers(; kwargs...))
     @info "Starting workers on SGE: 1"
-    for i in 1:nb_retries
-      try 
+    for i = 1:nb_retries
+      try
         schedule(task)
         wait(task)
         break
@@ -43,18 +43,18 @@ function init_workers(nb_retries=5;kwargs...)
     @everywhere begin
       using Pkg, Distributed
       using NLPModels
-    end 
-    
+    end
+
     #TODO: dispatch modules found in current environment:
 
     # Define function to instantiate problems on workers: 
     @everywhere workers() worker_problems = Vector{AbstractNLPModel}()
-    
-    @everywhere function push_worker_problems(problems:: Vector{P}) where {P <: AbstractNLPModel}
+
+    @everywhere function push_worker_problems(problems::Vector{P}) where {P <: AbstractNLPModel}
       global worker_problems
       push!(worker_problems, problems...)
     end
-    
+
     @everywhere function clear_worker_problems()
       global worker_problems
       worker_problems = Vector{AbstractNLPModel}()
