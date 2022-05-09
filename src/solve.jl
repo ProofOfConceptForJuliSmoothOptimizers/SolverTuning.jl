5f4984c08424c9ca7f83b50e25331fed5d571f06
Monssaf Toukal <tmonssaf@gmail.com>
2022-05-09 17:36:19 -0400

    improve output of solvertuning

--- src/solve.jl
+++ src/solve.jl
@@ -9,19 +9,21 @@ end
 
 function solve_with_nomad(bbmodel::AbstractBBModel; kwargs...)
   param_optimization_problem = ParameterOptimizationProblem(bbmodel)
-  let result = nothing
+  let result = nothing, best_params=nothing
     try
 
       # named arguments are options to pass to Nomad
       create_nomad_problem!(param_optimization_problem; kwargs...)
       result = solve_with_nomad!(param_optimization_problem)
       result = result.x_best_feas
+      best_params = (; zip(keys(param_opt_problem.nlp.meta.x_n), result)...)
+      @info "Best feasible parameters: $best_params"
     catch e
       @error "Error occured while running NOMAD: $e"
+      best_params = (; zip(keys(param_opt_problem.nlp.meta.x_n), param_optimization_problem.x)...)
     finally
-      @info ("Best feasible parameters: $(param_optimization_problem.x)")
       rmprocs(workers())
-      return result
+      return best_params
     end
   end
 end
